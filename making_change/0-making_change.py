c06f3411335c6450031d165422021d674b3530e0
Mcpe4ce <phil.ghanem@gmail.com>
2026-07-10 11:42:16 +0200

    fix: format conditional statement for better readability in makeChange function

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -24,9 +24,8 @@ def makeChange(coins, total):
 
     for amount in range(1, total + 1):
         for coin in coins:
-            if coin <= amount and min_coins[amount - coin] + 1 < min_coins[amount]:
+            if (coin <= amount and
+                    min_coins[amount - coin] + 1 < min_coins[amount]):
                 min_coins[amount] = min_coins[amount - coin] + 1
 
     return min_coins[total] if min_coins[total] != float('inf') else -1
-
-
